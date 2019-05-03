c12920c889e84d0f63e6dab50732a88b0945971a
frwa7342 <fredrik.waern@hotmail.com>
2019-05-03 09:40:44 +0200

    Added use static and wwwroot

--- MicroRPG/Controllers/GameController.cs
+++ MicroRPG/Controllers/GameController.cs
@@ -14,6 +14,7 @@ namespace MicroRPG.Controllers
             return View ();
         }
 
+        [Route("PlayerDetails")]
         public IActionResult _PlayerDetails()
         {
             return PartialView();
