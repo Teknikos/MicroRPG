78c840d2e2372b8d9e1518ce50311a27e3333117
frwa7342 <fredrik.waern@hotmail.com>
2019-05-05 17:24:04 +0200

    Tried to create a monster table and add their database parameters.

---
+++ RPGDB/Monsters.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Monsters]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Name] nvarchar (64) not null,
+	[EnvTags] nvarchar (64) not null,
+	[Description] nvarchar (128) not null,
+	[Speed] int not null,
+	[HP] int not null,
+	[Reduction] int not null,
+	[Damage] nvarchar (16) not null
+)
