271672b54914a02efcf19c044e656372521a9e57
Elias Gustafsson <gustafsson.ea@gmail.com>
2019-05-10 10:58:11 +0200

    Added dots to relations

--- MicroRPG/Models/Backstory/RelationTag.cs
+++ MicroRPG/Models/Backstory/RelationTag.cs
@@ -58,24 +58,24 @@ namespace MicroRPG.Models.Backstory
                 // Sibling
                 if (Math.Abs(player.Age - relatesTo.Age) < GrownUpAge)
                 {
-                    tag = CreateRelationTag("Sibling", " is your sibling", player, relatesTo, true);
+                    tag = CreateRelationTag("Sibling", " is your sibling.", player, relatesTo, true);
                     res.Add(tag);
                 }
                 // Parent / Child
                 else if (player.Age - relatesTo.Age >= GrownUpAge)
                 {
-                    tag = CreateRelationTag("Parent", " is your child", player, relatesTo, true, "Child", " is your parent");
+                    tag = CreateRelationTag("Parent", " is your child.", player, relatesTo, true, "Child", " is your parent".);
                     res.Add(tag);
                 }
                 // Cousins
-                tag = CreateRelationTag("Cousin", " is your distant cousin", player, relatesTo, true);
+                tag = CreateRelationTag("Cousin", " is your distant cousin.", player, relatesTo, true);
                 res.Add(tag);
             }
 
             // Infatuated
             if (player.Age - relatesTo.Age < GrownUpAge || relatesTo.Age > GrownUpAge)
             {
-                tag = new RelationTag("Infatuated", "You are in love with " + relatesTo.Name)
+                tag = new RelationTag("Infatuated", "You are in love with " + relatesTo.Name + ".")
                 {
                     RelatesToID = relatesTo.ID
                 };
@@ -83,73 +83,73 @@ namespace MicroRPG.Models.Backstory
             }
 
             // Cursed
-            tag = CreateRelationTag("Cursed", " and you have both been cursed", player, relatesTo);
+            tag = CreateRelationTag("Cursed", " and you have both been cursed.", player, relatesTo);
             res.Add(tag);
 
             // Brawn / Brains
-            tag = CreateRelationTag("Brawn", " is the brains and you are the brawn", player, relatesTo, false, "Brains", " is the brawn and you are the brains");
+            tag = CreateRelationTag("Brawn", " is the brains and you are the brawn.", player, relatesTo, false, "Brains", " is the brawn and you are the brains.");
             res.Add(tag);
 
             // Mercenary / Employer
-            tag = CreateRelationTag("Mercenary", " has hired you as a mercenary", player, relatesTo, false, "Employer", " is hired as your mercenary");
+            tag = CreateRelationTag("Mercenary", " has hired you as a mercenary.", player, relatesTo, false, "Employer", " is hired as your mercenary.");
             res.Add(tag);
 
             // Oathsworn
-            tag = CreateRelationTag("Oathsworn", " and you are bound by a sworn oath", player, relatesTo);
+            tag = CreateRelationTag("Oathsworn", " and you are bound by a sworn oath.", player, relatesTo);
             res.Add(tag);
 
             // Rivals
-            tag = CreateRelationTag("Rivals", " and you friendly rivals", player, relatesTo);
+            tag = CreateRelationTag("Rivals", " and you friendly rivals.", player, relatesTo);
             res.Add(tag);
 
             // Childhood Friends
             if (Math.Abs(player.Age - relatesTo.Age) < GrownUpAge)
             {
-                tag = CreateRelationTag("Friends", " and you are childhood friends", player, relatesTo);
+                tag = CreateRelationTag("Friends", " and you are childhood friends.", player, relatesTo);
                 res.Add(tag);
             }
 
             // Former co-workers
-            tag = CreateRelationTag("Co-workers", " and you are former co-workers", player, relatesTo);
+            tag = CreateRelationTag("Co-workers", " and you are former co-workers.", player, relatesTo);
             res.Add(tag);
 
             // Adventuring Companions
-            tag = CreateRelationTag("AdventuringCompanions", " and you are longtime adventuring companions", player, relatesTo);
+            tag = CreateRelationTag("AdventuringCompanions", " and you are longtime adventuring companions.", player, relatesTo);
             res.Add(tag);
 
             // Only survivors
-            tag = CreateRelationTag("Survivors", " and you were the only survivors of a disastrous event", player, relatesTo);
+            tag = CreateRelationTag("Survivors", " and you were the only survivors of a disastrous event.", player, relatesTo);
             res.Add(tag);
 
             // Treasure Map carriers
-            tag = CreateRelationTag("TreasureMap", " and you each carry half of a treasure map", player, relatesTo);
+            tag = CreateRelationTag("TreasureMap", " and you each carry half of a treasure map.", player, relatesTo);
             res.Add(tag);
 
             // Secret
-            tag = new RelationTag("Secret", $"You keep {relatesTo.Name} close because they know your secret")
+            tag = new RelationTag("Secret", $"You keep {relatesTo.Name} close because they know your secret.")
             {
                 RelatesToID = relatesTo.ID
             };
             res.Add(tag);
 
             // Brothers in arms
-            tag = CreateRelationTag("BrothersInArms", " and you are brothers in arms", player, relatesTo);
+            tag = CreateRelationTag("BrothersInArms", " and you are brothers in arms.", player, relatesTo);
             res.Add(tag);
 
             // Blood Debt
-            tag = CreateRelationTag("BloodDebt", " owes you a debt...  in blood", player, relatesTo, false, "Indebted", " has you owing them a debt... in blood");
+            tag = CreateRelationTag("BloodDebt", " owes you a debt...  in blood.", player, relatesTo, false, "Indebted", " has you owing them a debt... in blood.");
             res.Add(tag);
 
             // Trained
-            tag = CreateRelationTag("Trained", " and you trained together", player, relatesTo);
+            tag = CreateRelationTag("Trained", " and you trained together.", player, relatesTo);
             res.Add(tag);
 
             // Servants
-            tag = CreateRelationTag("Servants", " and you both serve the same supernatural power", player, relatesTo);
+            tag = CreateRelationTag("Servants", " and you both serve the same supernatural power.", player, relatesTo);
             res.Add(tag);
 
             // Dreamer / Savior
-            tag = CreateRelationTag("Dreamer", " was the one you dreamed would save your life, long before you met", player, relatesTo, false, "DreamSavior", " dreamed that you would save their life, long before you met");
+            tag = CreateRelationTag("Dreamer", " was the one you dreamed would save your life, long before you met.", player, relatesTo, false, "DreamSavior", " dreamed that you would save their life, long before you met.");
             res.Add(tag);
 
             return res;
